ac880cd56b167d4294f6b8fe9d3db67308914510
Helen <yuhan.yangg@gmail.com>
2021-10-26 19:37:37 +0800

    sidebar

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,9 +1,15 @@
 import Header from '../../header/Header';
+import Posts from '../../posts/Posts';
+import Sidebar from '../../sidebar/Sidebar';
 import './home.css';
 export default function Home() {
     return (
+        <>
+        <Header/>
         <div className="home">
-            <Header/>
+            <Posts/>
+            <Sidebar/>
         </div>
+        </>
     )
 }
---
+++ src/sidebar/Sidebar.js
@@ -0,0 +1,33 @@
+import './sidebar.css'
+
+export default function Sidebar() {
+    return (
+        <div className="sidebar">
+            <div className="sidebarItem">
+                <span className="sidebarTitle">About me</span>
+                <img src="https://themegoods-cdn-pzbycso8wng.stackpathdns.com/grandblog/demo/wp-content/uploads/2015/11/aboutme.jpg" alt="" />
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Commodi officia repudiandae ullam modi voluptates reiciendis vitae possimus hic repellendus at sequi provident maxime eaque dolorum veniam velit cumque, officiis ea?</p>
+            </div>
+            <div className="sidebarItem">
+                <span className="sidebarTitle">Categories</span>
+                <ul className="sidebarList">
+                    <li className="sidebarListItem">Life</li>
+                    <li className="sidebarListItem">Music</li>
+                    <li className="sidebarListItem">Style</li>
+                    <li className="sidebarListItem">Sport</li>
+                    <li className="sidebarListItem">Tech</li>
+                    <li className="sidebarListItem">Cinema</li>
+                </ul>
+            </div>
+            <div className="sidebarItem">
+                <span className="sidebarTitle">Follow us</span>
+                <div className="sidebarSocial">
+                    <i className="sidebarIcon fab fa-facebook-square"></i>
+                    <i className="sidebarIcon fab fa-twitter-square"></i>
+                    <i className="sidebarIcon fab fa-pinterest-square"></i>
+                    <i className="sidebarIcon fab fa-instagram-square"></i>
+                </div>
+            </div>
+        </div>
+    )
+}
