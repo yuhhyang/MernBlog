a9c5cedd14bdc80acd21022c839e7c24b44e90f4
Helen <yuhan.yangg@gmail.com>
2021-10-26 19:07:49 +0800

    header

---
+++ src/pages/home/Home.js
@@ -0,0 +1,9 @@
+import Header from '../../header/Header';
+import './home.css';
+export default function Home() {
+    return (
+        <div className="home">
+            <Header/>
+        </div>
+    )
+}
