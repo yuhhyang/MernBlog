da7e851ed544196f9756fd42b92959ebf17ad9e9
Helen <yuhan.yangg@gmail.com>
2021-11-18 02:53:13 +0800

    auth register routes

--- api/index.js
+++ api/index.js
@@ -2,7 +2,11 @@ const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
+const authRoute = require("./routes/auth");
+
 dotenv.config();
+//app.use(express.json()); //有這行才能顯示 json 資料
+
 mongoose.connect(process.env.MONGO_URL, {
     // Mongoose 6.0 拿掉 useNewUrlParser & useCreateIndex
     useUnifiedTopology: true
@@ -10,6 +14,7 @@ mongoose.connect(process.env.MONGO_URL, {
 .then(console.log("Connected to MongoDB"))
 .catch((err) => console.log(err));
 
+app.use("/api/auth", authRoute);
 
 app.listen("5050", () => {
     console.log("Backend is running.");
