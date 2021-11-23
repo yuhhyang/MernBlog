9b3f70746e6a4c50173f0d2b431f92aff7e52216
Helen <yuhan.yangg@gmail.com>
2021-11-23 18:21:50 +0800

    get all posts

--- api/routes/post.js
+++ api/routes/post.js
@@ -63,4 +63,26 @@ router.get("/:id", async (req, res) => {
         res.status(500).json(err);
     }
 });
+//get all posts
+router.get("/", async (req, res) => {
+    const username = req.query.user;
+    const catName = req.query.cat;
+    try {
+        let posts;
+        if (username) {
+            posts = await Post.find({ username });
+        } else if (catName) {
+        posts = await Post.find({
+            categories: {
+            $in: [catName],
+        },
+        });
+    } else {
+        posts = await Post.find();
+    }
+    res.status(200).json(posts);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 module.exports = router;
