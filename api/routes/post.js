9ead6f3c881c84c40b999f847f3b1d92d94de3b3
Helen <yuhan.yangg@gmail.com>
2021-11-23 18:14:53 +0800

    get post

--- api/routes/post.js
+++ api/routes/post.js
@@ -54,4 +54,13 @@ router.delete("/:id", async (req, res) => {
         res.status(500).json(err);
     }
 });
+//get post
+router.get("/:id", async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        res.status(200).json(post);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 module.exports = router;
