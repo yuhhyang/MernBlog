2a5aa18006608cb207850be51e7008bedfd8012f
Helen <yuhan.yangg@gmail.com>
2021-11-23 17:06:05 +0800

    delete user and posts

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const User = require("../models/User");
+const Post = require("../models/Post");
 const bcrypt = require("bcrypt");
 // Update User
 router.put("/:id", async (req, res) => {
@@ -22,6 +23,26 @@ router.put("/:id", async (req, res) => {
         }
     } else{
         res.status(401).json("You can update only your account")
+    }   
+});
+// Delete
+router.delete("/:id", async (req, res) => {
+    if(req.body.userId === req.params.id){
+        try{
+            const user = await User.findById(req.params.id);
+            try{
+                await User.findByIdAndDelete(req.params.id);
+                await Post.deleteMany({username: user.username});
+                // 刪除使用者時也刪除所有關於此使用者的貼文
+                res.status(200).json("User has been deleted.");
+            }catch(err){
+            res.status(500).json(err);
+        }
+        } catch(err) {
+            res.status(404).json("User not found");
+        }
+    } else{
+        res.status(401).json("You can delete only your account")
     }   
 });
 module.exports = router;
