bc601b4afc429746c71f719b7e53143e1b39cb3c
Helen <yuhan.yangg@gmail.com>
2021-11-23 17:15:03 +0800

    get user

--- api/routes/users.js
+++ api/routes/users.js
@@ -45,4 +45,14 @@ router.delete("/:id", async (req, res) => {
         res.status(401).json("You can delete only your account")
     }   
 });
+// Get user
+router.get("/:id", async (req, res) => {
+    try{
+        const user = await User.findById(req.params.id);
+        const { password, ...others } = user._doc;
+        res.status(200).json(others);
+    }catch (err){
+        res.status(500).json(err);
+    }
+});
 module.exports = router;
