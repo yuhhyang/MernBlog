d651a80f772500e31debbee078e3c196eeb0d46d
Helen <yuhan.yangg@gmail.com>
2021-11-23 18:04:38 +0800

    update post

--- api/routes/post.js
+++ api/routes/post.js
@@ -12,4 +12,28 @@ router.post("/", async (req, res) => {
         res.status(500).json(err);
     }
 });
+//update post
+router.put("/:id", async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        if (post.username === req.body.username) {
+        try {
+            const updatedPost = await Post.findByIdAndUpdate(
+            req.params.id,
+            {
+                $set: req.body,
+            },
+            { new: true }
+            );
+            res.status(200).json(updatedPost);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    } else {
+        res.status(401).json("You can update only your post!");
+    }
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 module.exports = router;
