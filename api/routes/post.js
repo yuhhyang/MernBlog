63914822b395abb692cc7cf5491c230669565a9f
Helen <yuhan.yangg@gmail.com>
2021-11-23 18:09:08 +0800

    delete post

--- api/routes/post.js
+++ api/routes/post.js
@@ -36,4 +36,22 @@ router.put("/:id", async (req, res) => {
         res.status(500).json(err);
     }
 });
+// delete post
+router.delete("/:id", async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        if (post.username === req.body.username) {
+        try {
+            await post.delete();
+            res.status(200).json("Post has been deleted...");
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    } else {
+        res.status(401).json("You can delete only your post!");
+    }
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 module.exports = router;
