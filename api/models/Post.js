a21634df214c7bc76bfed366faa0278df3653b87
Helen <yuhan.yangg@gmail.com>
2021-11-23 17:47:54 +0800

    add new post

--- api/models/Post.js
+++ api/models/Post.js
@@ -11,7 +11,7 @@ const PostSchema = new mongoose.Schema({
         },
         photo:{
             type: String,
-            required: true
+            required: false
         },
         username:{
             type: String,
