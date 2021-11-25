3e494ebd09f2cafb05865e305ab17c293737c3ac
Helen <yuhan.yangg@gmail.com>
2021-11-26 03:13:36 +0800

    upload image save to DB

--- api/index.js
+++ api/index.js
@@ -6,6 +6,7 @@ const authRoute = require("./routes/auth");
 const userRoute = require("./routes/users");
 const postRoute = require("./routes/post");
 const categoryRoute = require("./routes/categories"); 
+const multer = require("multer");
 dotenv.config();
 app.use(express.json()); //有這行才能顯示 json 資料
 
@@ -15,6 +16,19 @@ mongoose.connect(process.env.MONGO_URL, {
 })
 .then(console.log("Connected to MongoDB"))
 .catch((err) => console.log(err));
+
+const storage = multer.diskStorage({
+    destination:(req,file,cb) => {
+        cb(null, "images");
+    },
+    filename:(req,file,cb) => {
+        cb(null, "1.JPG");
+    }
+});
+const upload = multer({ storage: storage });
+app.post("/api/upload", upload.single("file"),(req,res) => {
+    res.status(200).json("File has been uploaded");
+})
 
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
