5bc4c989bb771cda1c665b84049c0b0b28ba4b78
Helen <yuhan.yangg@gmail.com>
2021-11-26 17:34:00 +0800

    show the all posts to page

--- client/src/pages/home/Home.js
+++ client/src/pages/home/Home.js
@@ -9,7 +9,7 @@ export default function Home() {
     useEffect(() => {
         const fetchPosts = async() => {
             const res = await axios.get("/post")
-            console.log(res);
+            setPosts(res.data)
         }
         fetchPosts()
     },[])
@@ -17,7 +17,7 @@ export default function Home() {
         <>
         <Header/>
         <div className="home">
-            <Posts/>
+            <Posts posts={posts}/>
             <Sidebar/>
         </div>
         </>
