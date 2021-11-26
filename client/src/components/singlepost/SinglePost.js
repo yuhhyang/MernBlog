6b3d84b5925b0e0d78b226a01423ac01af539cff
Helen <yuhan.yangg@gmail.com>
2021-11-26 19:10:01 +0800

    Fetch single post data

--- client/src/components/singlepost/SinglePost.js
+++ client/src/components/singlepost/SinglePost.js
@@ -1,22 +1,37 @@
+import { useEffect, useState } from 'react';
+import { useLocation } from 'react-router';
+import axios from 'axios';
 import './singlePost.css';
-
 export default function SinglePost() {
+    const location = useLocation();
+    const path = location.pathname.split("/")[2]; // /post/619cbd94013f5bb9ae22f5d6
+    const [post, setPost] = useState({});
+    useEffect(() => {
+        const getPost = async () => {
+            const res = await axios.get(`/post/${path}`);
+            setPost(res.data);
+        }
+        getPost();
+    }, [path])
     return (
         <div className="singlePost">
             <div className="singlePostWrapper">
-                <img src="https://picsum.photos/940/650?random=1" alt="" className="singlePostImg" />
+                {post.photo && (
+                    <img src={post.photo} alt="" className="singlePostImg" />
+                )}
+                
                 <h1 className="singlePostTitle">
-                    Lorem, ipsum dolor sit amet 
+                    {post.title}
                     <div className="singlePostEdit">
                         <i className="singlePostIcon far fa-edit"></i>
                         <i className="singlePostIcon far fa-trash-alt"></i>
                     </div>
                 </h1>
                 <div className="singlePostInfo">
-                    <span className="singlePostAuthor">Author: <b>Safak</b></span>
-                    <span className="singlePostDate">1 hour ago</span>
+                    <span className="singlePostAuthor">{post.username}<b>Safak</b></span>
+                    <span className="singlePostDate">{new Date(post.createdAt).toDateString()}</span>
                 </div>
-                <p className="singlePostDescription">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima aperiam maxime porro eos, illo non tenetur eaque repellat minus? Iure cumque fuga et quisquam esse est natus ducimus repudiandae nemo.lorem Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cupiditate laborum illum illo, veritatis soluta repellendus, vel quia quis id nulla asperiores dolore enim vero saepe sapiente iure corrupti facilis mollitia. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, fugiat placeat amet neque impedit nesciunt dolores animi natus quia nisi accusamus optio incidunt, necessitatibus aperiam molestiae illo nulla, nobis pariatur!</p>
+                <p className="singlePostDescription">{post.description}</p>
             </div>
         </div>
     )
