72655df6c8f1a052a5271155b908330e28c39074
Helen <yuhan.yangg@gmail.com>
2021-12-05 01:20:41 +0800

    current user context api

--- client/src/components/topbar/TopBar.js
+++ client/src/components/topbar/TopBar.js
@@ -1,7 +1,9 @@
 import './topbar.css';
 import { Link } from 'react-router-dom';
+import { Context } from '../../context/Context';
+import { useContext } from 'react';
 export default function TopBar() {
-    const user = true;
+    const { user } = useContext(Context);
     return (
         <div className="top">
             <div className="topLeft">
